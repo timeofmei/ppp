7bb816f650bdb2c1f91dc8d5728f9e96a3f58eb4
timeofmei <timeofmei@gmail.com>
2026-09-06 16:29:21 +0800

    add some cpp code

---
+++ c4/t2_4.3.2.cpp
@@ -0,0 +1,8 @@
+int area(int length, int width);
+
+int main()
+{
+    int x0 = arena(7,2);
+    int x1 = area(7);
+    int x2 = area("seven",2);
+}
---
+++ c4/t3_4.5.3.cpp
@@ -0,0 +1,32 @@
+#include "PPP.h"
+
+int area(int length, int width)
+{
+    if (length <= 0 || width <= 0)
+        return -1;
+    return length * width;
+}
+
+int framed_area(int x, int y)
+{
+    constexpr int frame_width = 2;
+    if (x - frame_width <= 0 || y - frame_width <= 0)
+        error("non-positive area() argument called by framed_area()");
+    return area(x - frame_width, y - frame_width);
+}
+
+int f(int x, int y, int z)
+{
+    int area1 = area(x, y);
+    if (area1 <= 0)
+        error("non-positive area");
+    int area2 = framed_area(3, z);
+    int area3 = framed_area(y, z);
+    double ratio = double(area1) / area3;
+    return ratio;
+}
+
+int main()
+{
+    cout << f(3, 6, 7) << '\n';
+}
